728cd7fe836eaf06265404b1d7d8044670f06adb
iamafif <83905165+1afiif@users.noreply.github.com>
2023-04-14 23:05:46 +0900

    ready to merge file

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './css/style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
